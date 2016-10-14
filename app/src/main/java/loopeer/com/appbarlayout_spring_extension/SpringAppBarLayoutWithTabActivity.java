b44932b28f6e7930c41e8e832a81d3bbc2c0c238
ToDou <yytodou123@gmail.com>
2016-10-14 15:02:54 +0800

    add the tab scrimhelper

--- app/src/main/java/loopeer/com/appbarlayout_spring_extension/SpringAppBarLayoutWithTabActivity.java
+++ app/src/main/java/loopeer/com/appbarlayout_spring_extension/SpringAppBarLayoutWithTabActivity.java
@@ -2,9 +2,11 @@ package loopeer.com.appbarlayout_spring_extension;
 
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.AppBarLayoutSpringBehavior;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.TabLayout;
+import android.support.design.widget.TabScrimHelper;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -20,6 +22,8 @@ public class SpringAppBarLayoutWithTabActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
 
         final RealtimeBlurView realtimeBlurView = (RealtimeBlurView) findViewById(R.id.real_time_blur_view);
 
@@ -37,5 +41,7 @@ public class SpringAppBarLayoutWithTabActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
         viewPager.setAdapter(new TabFragmentPagerAdapter(getSupportFragmentManager()));
+        TabScrimHelper tabScrimHelper = new TabScrimHelper(tabLayout, collapsingToolbarLayout);
+        appBarLayout.addOnOffsetChangedListener(tabScrimHelper);
     }
 }
---
+++ appbarspring/src/main/java/android/support/design/widget/TabScrimHelper.java
@@ -0,0 +1,113 @@
+package android.support.design.widget;
+
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.support.v4.graphics.ColorUtils;
+import android.support.v4.view.ViewCompat;
+import android.util.Log;
+
+public class TabScrimHelper implements AppBarLayout.OnOffsetChangedListener {
+    private static final String TAG = "TabScrimBehavior";
+    private static final int DEFAULT_SCRIM_ANIMATION_DURATION = 600;
+
+    private TabLayout mTabLayout;
+    private CollapsingToolbarLayout mToolbarLayout;
+    private long mScrimAnimationDuration;
+    private int mScrimAlpha;
+    private boolean mScrimsAreShown;
+    private ValueAnimatorCompat mScrimAnimator;
+    private final int mNormalColor;
+    private final int mSelectedColor;
+    private int mCollapseTabSelectTextColor;
+    private int mCollapseTabNormalTextColor;
+    private int mCollapseTabBackgroundColor;
+
+    public TabScrimHelper(TabLayout tabLayout, CollapsingToolbarLayout toolbarLayout) {
+        initDefault();
+        mTabLayout = tabLayout;
+        mToolbarLayout = toolbarLayout;
+        ColorStateList colorStateList = mTabLayout.getTabTextColors();
+        mSelectedColor = colorStateList.getColorForState(new int[]{android.R.attr.state_selected}, Color.parseColor("#3F51B5"));
+        mNormalColor = colorStateList.getDefaultColor();
+    }
+
+    @SuppressWarnings("unused")
+    public void setCollapseTabBackgroundColor(int collapseTabBackgroundColor) {
+        mCollapseTabBackgroundColor = collapseTabBackgroundColor;
+    }
+
+    @SuppressWarnings("unused")
+    public void setCollapseTabSelectTextColor(int collapseTabSelectTextColor) {
+        mCollapseTabSelectTextColor = collapseTabSelectTextColor;
+    }
+
+    private void initDefault() {
+        mScrimAnimationDuration = DEFAULT_SCRIM_ANIMATION_DURATION;
+        mCollapseTabBackgroundColor = Color.parseColor("#3F51B5");
+        mCollapseTabNormalTextColor = Color.parseColor("#FFFFFF");
+        mCollapseTabSelectTextColor = Color.parseColor("#FF4081");
+    }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+        Log.e(TAG, " " + verticalOffset);
+        setScrimsShown(mToolbarLayout.getHeight() + verticalOffset < mToolbarLayout.getScrimVisibleHeightTrigger());
+
+    }
+
+    public void setScrimsShown(boolean shown) {
+        setScrimsShown(shown, ViewCompat.isLaidOut(mToolbarLayout) && !mToolbarLayout.isInEditMode());
+    }
+
+    public void setScrimsShown(boolean shown, boolean animate) {
+        if (mScrimsAreShown != shown) {
+            if (animate) {
+                animateScrim(shown ? 0xFF : 0x0);
+            } else {
+                setScrimAlpha(shown ? 0xFF : 0x0);
+            }
+            mScrimsAreShown = shown;
+        }
+    }
+
+    private void animateScrim(int targetAlpha) {
+        if (mScrimAnimator == null) {
+            mScrimAnimator = ViewUtils.createAnimator();
+            mScrimAnimator.setDuration(mScrimAnimationDuration);
+            mScrimAnimator.setInterpolator(
+                    targetAlpha > mScrimAlpha
+                            ? AnimationUtils.FAST_OUT_LINEAR_IN_INTERPOLATOR
+                            : AnimationUtils.LINEAR_OUT_SLOW_IN_INTERPOLATOR);
+            mScrimAnimator.addUpdateListener(new ValueAnimatorCompat.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimatorCompat animator) {
+                    setScrimAlpha(animator.getAnimatedIntValue());
+                }
+            });
+        } else if (mScrimAnimator.isRunning()) {
+            mScrimAnimator.cancel();
+        }
+
+        mScrimAnimator.setIntValues(mScrimAlpha, targetAlpha);
+        mScrimAnimator.start();
+    }
+
+    void setScrimAlpha(int alpha) {
+        if (alpha != mScrimAlpha) {
+            mScrimAlpha = alpha;
+            updateLayout();
+        }
+    }
+
+    private void updateLayout() {
+        int color = ColorUtils.setAlphaComponent(mCollapseTabBackgroundColor, mScrimAlpha);
+        mTabLayout.setBackgroundColor(color);
+
+        float i = 1.f * mScrimAlpha / 255;
+        mTabLayout.setTabTextColors(
+                ColorUtils.blendARGB(mNormalColor, mCollapseTabNormalTextColor, i),
+                ColorUtils.blendARGB(mSelectedColor, mCollapseTabSelectTextColor, i));
+
+    }
+}
